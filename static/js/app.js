c1cd1756eef050e7cd33fe73fedb14c59c7e913f
Gadise Mulat <gtmulat@gmail.com>
2022-07-31 18:19:18 -0500

    hw 12

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,9 @@
+let jsonFile = "static/data/samples.json"
+
 function init() {
   var selector = d3.select("#selDataset");
 
-  d3.json("static/data/samples.json").then((data) => {
+  d3.json(jsonFile).then((data) => {
     console.log(data);    
     var sampleNames = data.names;
 
@@ -32,49 +34,90 @@ function optionChanged(newSample) {
 }
 
 function buildMetadata(sample) {
-
 // Use `d3.json` to fetch the sample data for the plots
-  d3.json("static/data/samples.json").then((data) => {
-    var metadata = data.metadata;
-    var resultArray= metadata.filter(sampleObj => sampleObj.id == sample);
-    var result= resultArray[0]
+  d3.json(jsonFile).then((data) => {
+    var metaData = data.metaData;
+    var resultArray = metaData.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
     var PANEL = d3.select("#sample-metadata");
 
     PANEL.html("");
     Object.entries(result).forEach(([key, value]) => {
-      PANEL.append("h6").text('${key.toUpperCase()}: ${value}');
+      d3.select("#sample-metadata")
+      .append("h5").text('${key}: ${value}');
     });
   });
 }
 
-  var ids = result.otu_ids;
-  var labels = result.otu_labels;
-  var values = result.sample_values;
-  const firstSample = sampleNames[0];
-  buildCharts(firstSample);
-  buildMetadata(firstSample);
+function buildBarChart(sample) {
+  d3.json(jsonFile).then((data) => {
+      var samples = data.samples;
+      var sample_data = samples.filter(x => x.id == sample);
+      console.log(sample_data);
+
+      sample_data = sample_data[0];
+
+      var otu_ids = sample_data.otu_ids;
+      var otu_labels = sample_data.otu_labels;
+      var sample_values = sample_data.sample_values;
+
+      var yticks = otu_ids.map(x => `OTU ${x}`);
+
+      var trace1 = {
+          x: sample_values.slice(0, 10).reverse(),
+          y: yticks.slice(0, 10).reverse(),
+          hovertext: otu_labels.slice(0, 10).reverse(),
+          name: 'Top Bacteria',
+          orientation: "h",
+          marker: {
+              color: "firebrick"
+          },
+          type: 'bar'
+      }
+      var barData = [trace1]; 
+      var barLayout = {
+          title: "Top Bacteria Bar Chart",
+          xaxis: { title: "Count of Bacteria" }
+      };
+      Plotly.newPlot('bar', barData, barLayout);
   });
 }
 
-d3.selectAll("#dropdownMenu").on("change", updatePlotly);
-function updatePlotly(); {
-  var dropdownMenu = d3.select("#dropdownMenu");
-  var dataset = dropdownMenu.property("value");
+function buildGaugeChart(sample) {
+  d3.json(jsonFile).then((data) => {
+      var metadata = data.metadata;
+      var metadata_data = metadata.filter(x => x.id == sample);
+      metadata_data = metadata_data[0];
 
-  var xData = [1, 2, 3, 4, 5];
-  var yData = [];
 
-  if (dataset === 'dataset1') {
-    yData = [1, 2, 4, 8, 16];
-  };
+      var wfreq = metadata_data.wfreq;
 
-  if (dataset === 'dataset2') {
-    yData = [1, 10, 100, 1000, 10000];
-  };
+      var trace1 = {
+          domain: { x: [0, 1], y: [0, 1] },
+          value: wfreq,
+          title: { text: "Washing Frequence" },
+          type: "indicator",
+          mode: "gauge+number+delta",
+          delta: { reference: 5 },
+          gauge: {
+              axis: { range: [null, 10] },
+              steps: [
+                  { range: [0, 4], color: "lightgray" },
+                  { range: [4, 8], color: "gray" }
+              ],
+              threshold: {
+                  line: { color: "red", width: 4 },
+                  thickness: 0.75,
+                  value: 9.5
+              }
+          }
+      };
+      var gaugeData = [trace1];
+      var gaugeLayout = {
+          title: "Washing Frequency Gauge Chart"
+      };
 
-  var trace = {
-    x: [xData],
-    y: [yData],
-  };
-  Plotly.restyle("plot", trace);
-};
+      Plotly.newPlot('gauge', gaugeData, gaugeLayout);
+  });
+
+}
